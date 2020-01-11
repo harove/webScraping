8334965f0c33da01d3d0d4d0e3d035a7bc938bb3
edgar <harove@gmail.com>
2020-01-10 21:06:18 -0300

    scraping using cheerio and selenium webdriver

---
+++ scraping.js
@@ -0,0 +1,479 @@
+const request = require('request'); 
+
+const cheerio = require('cheerio'); 
+
+const fs = require('fs'); 
+
+var FormData = require('form-data'); 
+
+const axios = require('axios').default; 
+
+require('axios-response-logger'); 
+
+const {Builder, By, Key, until} = require('selenium-webdriver'); 
+
+var lineReader = require('line-reader'); 
+
+ 
+
+ 
+
+// axios.interceptors.request.use(request => { 
+
+//     console.log('Starting Request', request) 
+
+//     return request 
+
+//   }) 
+
+   
+
+//   axios.interceptors.response.use(response => { 
+
+//     console.log('Response:', response) 
+
+//     return response 
+
+//   }) 
+
+ 
+
+ 
+
+ 
+
+  
+
+// var form = new FormData(); 
+
+// form.append('ctl00$MainContent$cboDenominacionSocial', '198'); 
+
+ 
+
+ 
+
+ 
+
+ 
+
+// body.append('email', 'emailId'); 
+
+// body.append('password', 'xyz'); 
+
+// this.http.post(url, body); 
+
+ 
+
+ 
+
+request('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56',(error, 
+
+ response,html)=>{ 
+
+    if (!error && response.statusCode == 200) { 
+
+        //console.log(html); 
+
+        const $ = cheerio.load(html); 
+
+ 
+
+ 
+
+        const options = $('select[name ="ctl00$MainContent$cboDenominacionSocial"] option') 
+
+ 
+
+ 
+
+  
+
+ 
+
+ 
+
+            //console.log(options[0].children); 
+
+            //console.log(options[100].children[0].data) 
+
+ 
+
+ 
+
+            // options.map((i,el)=>{ 
+
+            //     console.log($(this)); 
+
+            // }) 
+
+ 
+
+ 
+
+            //console.log(options[1].children[0].data) 
+
+ 
+
+ 
+
+            // options.map((i,e)=>{ 
+
+            //     console.log(e.children[0].data) 
+
+            // }); 
+
+ 
+
+ 
+
+            // fs.writeFile('Output.txt',"hola",(err)=>{ 
+
+            //     if (err) throw err; 
+
+            // }); 
+
+ 
+
+ 
+
+            // fs.truncate('Output.txt',0,(err)=>{ 
+
+            //     if (err) throw err; 
+
+            // }); 
+
+ 
+
+ 
+
+            options.map((i,e)=>{ 
+
+                fs.appendFile('Output.txt', i + ',' + e.attribs.value + ',' + e.children[0].data + '\n', (err) => {  
+
+       
+
+                // //     // In case of a error throw err.  
+
+                // //     if (err) throw err;  
+
+                })  
+
+              
+
+            }); 
+
+    } 
+
+}); 
+
+ 
+
+ 
+
+ 
+
+ 
+
+const getElement = (id,driver,nextChr)=>{ 
+
+    driver.findElement(By.id(id)).getText().then((txt)=>{ 
+
+       //console.log(txt) 
+
+        fs.appendFile('company.txt',txt+nextChr,(err)=>{ 
+
+            if (err) throw err; 
+
+        }) 
+
+    }) 
+
+} 
+
+ 
+
+ 
+
+ 
+
+(function example() { 
+
+    let driver = new Builder().forBrowser('chrome').build(); 
+
+    try { 
+
+ 
+
+ 
+
+      fs.truncate('company.txt', 0, function(){console.log('done')}) 
+
+       
+
+     
+
+       driver.get('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56').then( ()=>{ 
+
+        const company = driver.findElement(By.id("MainContent_cboDenominacionSocial")).sendKeys('AUSTRAL GROUP').then(()=>{ 
+
+       driver.wait(until.elementLocated(By.id('MainContent_txtCIUU')),10000).then(()=>{ 
+            getElement('MainContent_lblCodigoRPJ',driver,','); 
+            getElement('MainContent_lblRazonSocial',driver,','); 
+            getElement('MainContent_lblCIUU',driver,'\n'); 
+            getElement('MainContent_txtCodigoRPJ',driver,','); 
+            getElement('MainContent_txtRazonSocial',driver,','); 
+            getElement('MainContent_txtCIUU',driver,','); 
+         }).catch((err)=>console.log(err)); 
+        }) 
+       }); 
+
+
+
+
+    //   lineReader.eachLine('Output.txt', async function(line, last) { 
+
+ 
+
+ 
+
+    //         const item = line.substr(line.lastIndexOf(",")+1,line.lastIndexOf(",")+10) 
+
+ 
+
+ 
+
+ 
+
+    //         await driver.get('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56').then(async ()=>{ 
+
+         
+
+ 
+
+ 
+
+ 
+
+ 
+
+    //           const company = await driver.findElement(By.id("MainContent_cboDenominacionSocial")).sendKeys(item).then(()=>{ 
+
+    //             driver.wait(until.elementLocated(By.id('MainContent_txtCIUU')),10000).then(()=>{ 
+
+    //               console.log(item); 
+
+    //               getElement('MainContent_lblCodigoRPJ',driver,','); 
+
+    //               getElement('MainContent_lblRazonSocial',driver,','); 
+
+    //               getElement('MainContent_lblCIUU',driver,'\n'); 
+
+           
+
+    //               getElement('MainContent_txtCodigoRPJ',driver,','); 
+
+    //               getElement('MainContent_txtRazonSocial',driver,','); 
+
+    //               getElement('MainContent_txtCIUU',driver,','); 
+
+                  
+
+    //             }).catch((err)=>console.log(err)); 
+
+    //           }) 
+
+ 
+
+ 
+
+               
+
+    //          }); 
+
+    //     // do whatever you want with line... 
+
+    //     if(last){ 
+
+    //       // or check if it's the last one 
+
+    //     } 
+
+ 
+
+ 
+
+    //   }); 
+
+ 
+
+ 
+
+ 
+
+    //   await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN); 
+
+    //   await driver.wait(until.titleIs('webdriver - Google Search'), 1000); 
+
+     } finally { 
+
+    //    await driver.quit(); 
+
+     } 
+
+  })(); 
+
+ 
+
+ 
+
+ 
+
+ 
+
+   
+
+ 
+
+ 
+
+  //   request('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56',(error, 
+
+  //   response,html)=>{ 
+
+  //      if (!error && response.statusCode == 200) { 
+
+  //          //console.log(html); 
+
+  //          const $ = cheerio.load(html); 
+
+  //      } 
+
+  //  }); 
+
+ 
+
+ 
+
+ 
+
+ 
+
+  
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+    // axios.post('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56', { 
+
+    //     data:{"ctl00$MainContent$cboDenominacionSocial":33862} 
+
+    // }) 
+
+    // .then(function (response) { 
+
+    //     console.log(response.statusCode); 
+
+    // }) 
+
+    // .catch(function (error) { 
+
+    //     console.log(error); 
+
+    // }); 
+
+ 
+
+ 
+
+ 
+
+//   axios({ 
+
+//     method: 'post', 
+
+//     url: 'http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56', 
+
+//     headers:{ 
+
+//         //Cookie: "FlagLanguage=es; ASP.NET_SessionId=ypzktjuzc0ojxcs0horax2po; _ga=GA1.3.2067525774.1578325613; _gid=GA1.3.1389388398.1578325613" 
+
+//     },  
+
+//     data: { 
+
+//         ctl00$MainContent$cboDenominacionSocial:122466 
+
+//     }, 
+
+//   }).then(function (response) { 
+
+//         console.log(response); 
+
+//    }) 
+
+ 
+
+ 
+
+ 
+
+// request.post('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56', { 
+
+//     data: { 
+
+//         "ctl00$MainContent$cboDenominacionSocial":131703 
+
+//     } 
+
+ 
+
+ 
+
+// }, (error, res, body) => { 
+
+//   if (error) { 
+
+//     console.error(error) 
+
+//     return 
+
+//   } 
+
+//   console.log(`statusCode: ${res.statusCode}`) 
+
+//   console.log(`body: ${res.body}`) 
+
+ 
+
+ 
+
+//   //console.log(body) 
+
+// }) 
+
+ 
+
+ 
+
+ 
+
+ 
+
+// form.submit('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56', function(err, res) { 
+
+//   if (err) throw err 
+
+//   else 
+
+//   console.log(`body: ${res.statusCode}`) 
+
+// }); 
+
+ 
