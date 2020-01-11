329209bf54216ebb0c7d7e53c825a614afaa05a0
edgar <harove@gmail.com>
2020-01-11 17:16:11 -0300

    scraping working without foreach loops

--- scraping.js
+++ scraping.js
@@ -1,104 +1,41 @@
 const request = require('request'); 
-
 const cheerio = require('cheerio'); 
-
 const fs = require('fs'); 
-
 var FormData = require('form-data'); 
-
 const axios = require('axios').default; 
-
 require('axios-response-logger'); 
-
 const {Builder, By, Key, until} = require('selenium-webdriver'); 
-
 var lineReader = require('line-reader'); 
 
- 
-
- 
 
 // axios.interceptors.request.use(request => { 
-
 //     console.log('Starting Request', request) 
-
 //     return request 
-
 //   }) 
-
-   
 
 //   axios.interceptors.response.use(response => { 
-
 //     console.log('Response:', response) 
-
 //     return response 
-
 //   }) 
 
- 
-
- 
-
- 
-
-  
 
 // var form = new FormData(); 
 
 // form.append('ctl00$MainContent$cboDenominacionSocial', '198'); 
-
- 
-
- 
-
- 
-
- 
-
 // body.append('email', 'emailId'); 
-
 // body.append('password', 'xyz'); 
-
 // this.http.post(url, body); 
 
- 
-
- 
 
 request('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56',(error, 
-
  response,html)=>{ 
-
     if (!error && response.statusCode == 200) { 
-
         //console.log(html); 
-
         const $ = cheerio.load(html); 
-
- 
-
- 
-
         const options = $('select[name ="ctl00$MainContent$cboDenominacionSocial"] option') 
 
- 
-
- 
-
-  
-
- 
-
- 
-
             //console.log(options[0].children); 
-
             //console.log(options[100].children[0].data) 
-
- 
-
- 
 
             // options.map((i,el)=>{ 
 
@@ -106,209 +43,98 @@ request('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F
 
             // }) 
 
- 
-
- 
-
             //console.log(options[1].children[0].data) 
-
- 
-
- 
-
             // options.map((i,e)=>{ 
-
             //     console.log(e.children[0].data) 
-
             // }); 
-
- 
-
- 
 
             // fs.writeFile('Output.txt',"hola",(err)=>{ 
-
             //     if (err) throw err; 
-
             // }); 
-
- 
-
- 
 
             // fs.truncate('Output.txt',0,(err)=>{ 
-
             //     if (err) throw err; 
-
             // }); 
 
- 
-
- 
-
-            options.map((i,e)=>{ 
-
-                fs.appendFile('Output.txt', i + ',' + e.attribs.value + ',' + e.children[0].data + '\n', (err) => {  
-
-       
-
-                // //     // In case of a error throw err.  
-
-                // //     if (err) throw err;  
-
-                })  
-
-              
-
-            }); 
-
+            // options.map((i,e)=>{ 
+            //     fs.appendFile('Output.txt', i + ',' + e.attribs.value + ',' + e.children[0].data + '\n', (err) => {  
+            //     })
+            // }); 
     } 
-
 }); 
 
  
-
- 
-
- 
-
- 
-
 const getElement = (id,driver,nextChr)=>{ 
-
     driver.findElement(By.id(id)).getText().then((txt)=>{ 
-
        //console.log(txt) 
-
         fs.appendFile('company.txt',txt+nextChr,(err)=>{ 
-
             if (err) throw err; 
-
         }) 
-
     }) 
-
 } 
 
- 
-
- 
-
- 
-
-(function example() { 
-
-    let driver = new Builder().forBrowser('chrome').build(); 
-
+(async function example() { 
+    let driver = await new Builder().forBrowser('chrome').build(); 
+    
     try { 
+    
+        fs.truncate('company.txt', 0, function(){console.log('done')}) 
 
- 
+        // driver.get('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56').then( ()=>{ 
+        //     const company = driver.findElement(By.id("MainContent_cboDenominacionSocial")).sendKeys('AUSTRAL GROUP').then(()=>{ 
+        //         driver.wait(until.elementLocated(By.id('MainContent_txtCIUU')),10000).then(()=>{ 
+        //             getElement('MainContent_lblCodigoRPJ',driver,','); 
+        //             getElement('MainContent_lblRazonSocial',driver,','); 
+        //             getElement('MainContent_lblCIUU',driver,'\n'); 
+        //             getElement('MainContent_txtCodigoRPJ',driver,','); 
+        //             getElement('MainContent_txtRazonSocial',driver,','); 
+        //             getElement('MainContent_txtCIUU',driver,','); 
+        //         }).catch((err)=>console.log(err)); 
+        //     }) 
+        // }); 
 
- 
 
-      fs.truncate('company.txt', 0, function(){console.log('done')}) 
 
-       
+
+    // lineReader.eachLine('Output.txt', async function(line, last) { 
 
      
 
-       driver.get('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56').then( ()=>{ 
-
-        const company = driver.findElement(By.id("MainContent_cboDenominacionSocial")).sendKeys('AUSTRAL GROUP').then(()=>{ 
-
-       driver.wait(until.elementLocated(By.id('MainContent_txtCIUU')),10000).then(()=>{ 
-            getElement('MainContent_lblCodigoRPJ',driver,','); 
-            getElement('MainContent_lblRazonSocial',driver,','); 
-            getElement('MainContent_lblCIUU',driver,'\n'); 
-            getElement('MainContent_txtCodigoRPJ',driver,','); 
-            getElement('MainContent_txtRazonSocial',driver,','); 
-            getElement('MainContent_txtCIUU',driver,','); 
-         }).catch((err)=>console.log(err)); 
-        }) 
-       }); 
-
-
-
-
-    //   lineReader.eachLine('Output.txt', async function(line, last) { 
-
- 
-
- 
-
-    //         const item = line.substr(line.lastIndexOf(",")+1,line.lastIndexOf(",")+10) 
-
- 
-
- 
-
- 
-
-    //         await driver.get('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56').then(async ()=>{ 
-
-         
-
- 
-
- 
-
- 
-
- 
-
-    //           const company = await driver.findElement(By.id("MainContent_cboDenominacionSocial")).sendKeys(item).then(()=>{ 
-
-    //             driver.wait(until.elementLocated(By.id('MainContent_txtCIUU')),10000).then(()=>{ 
-
-    //               console.log(item); 
-
-    //               getElement('MainContent_lblCodigoRPJ',driver,','); 
-
-    //               getElement('MainContent_lblRazonSocial',driver,','); 
-
-    //               getElement('MainContent_lblCIUU',driver,'\n'); 
-
-           
-
-    //               getElement('MainContent_txtCodigoRPJ',driver,','); 
-
-    //               getElement('MainContent_txtRazonSocial',driver,','); 
-
-    //               getElement('MainContent_txtCIUU',driver,','); 
-
+                // var lineReader = require('readline').createInterface({
+                //     input: require('fs').createReadStream('Output.txt')
+                //   });
                   
-
-    //             }).catch((err)=>console.log(err)); 
-
-    //           }) 
-
- 
-
- 
-
+                //   lineReader.on('line', async function (line) {
+                        let item = 'ADMINISTRADORA DEL'
+                        console.log('Line from file:', item);
+                        //let item = line.substr(line.lastIndexOf(",")+1,line.lastIndexOf(",")+15) 
+                        await driver.get('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56')
+                        console.log(item);
+                        await driver.findElement(By.id("MainContent_cboDenominacionSocial")).sendKeys(item)
+                        await driver.wait(until.elementLocated(By.id('MainContent_txtCIUU')),10000)
+                        
+                        item = 'AFP HABITAT S.A.'
+                        console.log('Line from file:', item);
+                        //let item = line.substr(line.lastIndexOf(",")+1,line.lastIndexOf(",")+15) 
+                        await driver.get('http://www.smv.gob.pe/Frm_LisDatosGenerales?data=B5C77F8C6BF989E2D2220F8ED366A4EF1CB0260F56')
+                        console.log(item);
+                        await driver.findElement(By.id("MainContent_cboDenominacionSocial")).sendKeys(item)
+                        await driver.wait(until.elementLocated(By.id('MainContent_txtCIUU')),10000)
                
 
-    //          }); 
 
-    //     // do whatever you want with line... 
+                    
+                    // getElement('MainContent_lblCodigoRPJ',driver,',');
 
-    //     if(last){ 
+                //   });
 
-    //       // or check if it's the last one 
 
-    //     } 
+                //let item = 'ADMINISTRADORA DEL'
 
- 
-
- 
+               
+       
 
     //   }); 
-
- 
-
- 
-
- 
 
     //   await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN); 
 
